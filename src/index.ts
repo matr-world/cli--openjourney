72b158f12a754e6e8697afa4459bdf9881dd651a
qudo <59782726+qudo-code@users.noreply.github.com>
2023-03-28 20:06:56 -0700

    it works

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-import { User } from "@/types";
+#!/usr/bin/env node
+import chalk from "chalk";
+// @ts-ignore
+import midjourney from "midjourney-client";
+import open from "open";
 
-export default () => {
-    const user: User = {
-        id: 1,
-        name: "John Doe",
-    };
+(async () => {
+    const [_, __, ...prompt] = process.argv;
 
-    return user;
-};
+    const promptString = prompt.join(" ");
+
+    // eslint-disable-next-line no-console
+    console.log(`
+--------------------------------
+        OPENJOURNEY AI
+--------------------------------`);
+
+    // eslint-disable-next-line no-console
+    console.log(
+        `⚡️ Generateing Openjourney image...\n💬 Prompt: ${
+            promptString
+                ? chalk.bold.blue(promptString)
+                : chalk.bold.red("No prompt provided.")
+        }\n`
+    );
+
+    const [result] = await midjourney(promptString);
+
+    // eslint-disable-next-line no-console
+    console.log(`🚀 Generation success! \n🔗 ${result}`);
+
+    // eslint-disable-next-line no-console
+    console.log(`🌐 Opening result in browser...\n\n`);
+
+    open(result);
+})();
